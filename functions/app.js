f77abb4c9e57554a36d82e96dc6f7d200926962b
Feril Sunu <ferilsunu@gmail.com>
2024-02-10 01:29:16 +0530

    #3

--- functions/app.js
+++ functions/app.js
@@ -5,25 +5,49 @@ const path = require('path');
 const dotenv = require('dotenv');
 const hbs = require('express-handlebars');
 const router = express.Router();
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
-const public_path = path.join(__dirname, '/public');
+const public_path = path.join(__dirname, 'public');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(public_path));
 
-const indexRoute = require('../routers/index');
-const checkoutRoute = require('../routers/checkout');
-
 app.engine('handlebars', hbs({ defaultLayout: 'index' }));
 app.set('view engine', 'handlebars');
 
-app.use('/', indexRoute);
-app.use('/checkout', checkoutRoute);
+router.get('/', (req, res) => {
+    res.render('index');
+});
 
-app.use('/.netlify/functions/app',router);
+router.post('/', async (req, res) => {
+    try {
+        const session = await stripe.checkout.sessions.create({
+            success_url: `${process.env.DOMAIN}/success.html`,
+            cancel_url: `${process.env.DOMAIN}/cancel.html`,
+            payment_method_types: ['card'],
+            line_items: [{
+                price_data: {
+                    currency: 'inr',
+                    product_data: {
+                        name: 'Apple Gift Card'
+                    },
+                    unit_amount:  25000
+                },
+                quantity:  1
+            }],
+            mode: 'payment',
+            billing_address_collection: 'required'
+        });
+
+        res.json(session);
+    } catch (error) {
+        res.status(500).send('Error creating Stripe session');
+    }
+});
+
+app.use('/', router);
 
 module.exports.handler = serverless(app);
--- routers/checkout.js
+++ routers/checkout.js
@@ -1,33 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const stripe = require('stripe')(process.env.STRIPE_SECRET)
-
-router
-    .post('/', async (req, res) => {
-
-        const session = await stripe.checkout.sessions.create({
-            success_url: `${process.env.DOMAIN}/success.html`,
-            cancel_url: `${process.env.DOMAIN}/cancel.html`,
-            payment_method_types: ['card'],
-            line_items: [{
-                price_data: {
-                    currency: 'inr',
-                    product_data: {
-                        name: 'Apple Gift Card'
-                    },
-                    unit_amount: 25000
-                },
-                quantity: 1
-            }],
-            mode: 'payment',
-            billing_address_collection: 'required'
-        }) 
-
-        res.json(session)
 
 
-
-
-    })
 
 module.exports = router
